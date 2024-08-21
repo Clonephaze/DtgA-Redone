7dbbed74b95690bbbaab703037efde49f9a4838f
Jack Smith <jackdsmith2327@gmail.com>
2024-08-21 11:35:59 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Constants for the service worker
 const APP_PREFIX = 'DtgA_';  // Identifier for this app
-const VERSION = 'version_26'; // Version of the offline cache, itterate this number if changes are made and old caches should be cleared
+const VERSION = 'version_27'; // Version of the offline cache, itterate this number if changes are made and old caches should be cleared
 const CACHE_NAME = APP_PREFIX + VERSION; // Cache name combining app prefix and version
 const REPOSITORY = '/DtgA-Redone'; // Base path for repository, required for making a service worker work for github pages.
 const MAX_AGE = 24 * 60 * 60 * 1000; // Max age for cached resources in milliseconds (24 hours)
@@ -24,7 +24,7 @@ const URLS = [
     '/Assets/js/TableOfContentsGen.js',
     '/Assets/js/Utilities.js',
     '/Assets/js/OtherCreatorsJs/iro.min.js',
-    '/Assets/js/OtherCreatorsJs/tsparticles.bundle.min.js',
+    '/Assets/js/OtherCreatorsJs/tsparticles.basic.bundle.min.js',
     '/Assets/js/generationData.json',
     '/Assets/DtgA-logo_Final-GaplessBW.webp',
     '/Assets/Fonts/HyliaSerifBeta-Regular.otf',
@@ -121,6 +121,7 @@ const URLS = [
     '/Icons/ItemPlaceholder.webp',
     '/Icons/LanayruWaterfall.webp',
     '/Icons/Lancelot.webp',
+    '/Icons/Lantern.webp',
     '/Icons/LargeSword1.webp',
     '/Icons/LargeSword2.webp',
     '/Icons/LargeSword3.webp',
