560b6e75deedb4c148a85a429945dad2d3339e7c
Jack Smith <jackdsmith2327@gmail.com>
2024-08-27 23:51:55 -0700

    New Style for card popovers.

--- Assets/js/PopOvers.js
+++ Assets/js/PopOvers.js
@@ -239,9 +239,9 @@ function searchJson(jsonData, path, title) {
 function createPopoverContent(item) {
 	return `
 	<div class="card-pop-wrapper">
-		<p>${item.description}</p>	
-		<img src="${item.imageSrc}" alt="${item.title}" />
 		<h3>${item.title}</h3>
+		<img src="${item.imageSrc}" alt="${item.title}" />
+		<p>${item.description}</p>	
 	</div>
 	`;
 }
